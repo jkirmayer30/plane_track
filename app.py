0131250bc91f00dc473a3bf88668eba2237b3743
Jacob Kirmayer <jacobkirmayer@gmail.com>
2021-06-06 19:18:48 -0400

    handling inactive flights

--- app.py
+++ app.py
@@ -21,40 +21,51 @@ def get_html():
     
     html+='''<form action="http://127.0.0.1:5000">
   <label for="airline">Airline ID:</label><br>
+  <input type="text" id="airline" name="airline" value='''+airline_id+'''><br>
+  <label for="number">Flight Num:</label><br>
+  <input type="text" id="number" name="number" value='''+flight_number+'''><br><br>
+  <input type="submit" value="Submit">
+</form> 
+
+'''
+    if airline_id!='default' and airline_id!='':
+        try:
+            flight = []
+            for key in flights:
+                info = flights[key]
+                if type(info)==list:
+                    if info[-3][3:]==str(flight_number):
+                        flight = info
+                        break
+            html+='<p>'
+            html+='Altitude:' + str(flight[4])
+            html+='</p> <p>'
+            html+='Speed:' + str(flight[5])
+            html+='</p> <p>'
+            html+='Heading:' + str(flight[3])
+            html+='</p> <p>'
+            html+='Aircraft:' + str(flight[8])
+            html+='</p> <p>'
+            html+='Latitude:' + str(flight[1])
+            html+='</p> <p>'
+            html+='Longitude:' + str(flight[2])
+            html+='</p> <p>'
+            html+='Origin:' + str(flight[11])
+            html+='</p> <p>'
+            html+='Destination:' + str(flight[12])
+            html+='</p>'
+        except:
+            html ='''<!DOCTYPE html>
+            <html>'''
+            html+='''<form action="http://127.0.0.1:5000">
+  <label for="airline">Airline ID:</label><br>
   <input type="text" id="airline" name="airline"><br>
   <label for="number">Flight Num:</label><br>
   <input type="text" id="number" name="number"><br><br>
   <input type="submit" value="Submit">
 </form> 
-
 '''
-    if airline_id!='default':
-        flight = []
-        for key in flights:
-            info = flights[key]
-            if type(info)==list:
-                if info[-3][3:]==str(flight_number):
-                    flight = info
-                    break
-        print(flight)
-        html+='<p>'
-        html+='Altitude:' + str(flight[4])
-        html+='</p> <p>'
-        html+='Speed:' + str(flight[5])
-        html+='</p> <p>'
-        html+='Heading:' + str(flight[3])
-        html+='</p> <p>'
-        html+='Aircraft:' + str(flight[8])
-        html+='</p> <p>'
-        html+='Latitude:' + str(flight[1])
-        html+='</p> <p>'
-        html+='Longitude:' + str(flight[2])
-        html+='</p> <p>'
-        html+='Origin:' + str(flight[11])
-        html+='</p> <p>'
-        html+='Destination:' + str(flight[12])
-        html+='</p>'
-        html+='</html>'
+            html+='<p>This flight is not active </p></html>'
     
     return html
   
