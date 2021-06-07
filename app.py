ba7b66cb516aba91b81d37f685c6e6962e5468bf
Jacob Kirmayer <jacobkirmayer@gmail.com>
2021-06-07 10:28:41 -0400

    updates to info displayed

--- app.py
+++ app.py
@@ -1,16 +1,43 @@
 from flask import Flask,request
 import flightradar24
 app = Flask(__name__)
-  
+pop_file = open("planes.txt")
+text = pop_file.read()
+
+def get_data(s):
+    data = []
+    string = s[s.find('\n'):]
+    new = string.split('\n')
+    for x in new :
+        if x != '' :
+            data.append(x.split(','))
+    return data
+plane_types = get_data(text)
 @app.route('/',methods=['GET', 'POST'])
 def get_html():
     fr = flightradar24.Api()
     full_path = request.full_path
     airline_id=''
     flight_number = ''
+    def airport_name(n) :
+        airports = fr.get_airports()['rows']
+        for x in airports :
+            if x['iata'] == n :
+                return x['name'] + ', ' + x['country']
+    def airplane_type (n) :
+        print(n)
+        def unquote (m) :
+            return m.split('"')[1]
+        for x in plane_types :
+            if '"' in x[2] and unquote(x[2]) == n :
+                print(x[2])
+                return unquote(x[0])
+           
+            
+        
     try:
         airline_id  = full_path.split('airline=')[1].split('&')[0].upper()
-        print(airline_id)
+        
         flight_number  = full_path.split('number=')[1]
     except:
         print()
@@ -27,40 +54,39 @@ def get_html():
                         flight = info
                         break
             other_id = flight[13]
-            print(flight)
             image = ''
             for plane in fr.get_flight(other_id)['result']['response']['aircraftImages']:
-                if 'medium' in plane['images'] and len(plane['images']['medium'])>0 and plane['registration']==flight[9]:
+                if 'medium' in plane['images'] and len(plane['images']['medium'])>0:
                     image = plane['images']['medium'][0]['src']
-            print(image)
+                    if plane['registration']==flight[9]:
+                        break
             html +='''<!DOCTYPE html>
     <html><body style="background-image:url('''+image+''');">'''
     
             html+='''<div style="background-color:white;width:500px;padding:20px;border-radius:10px"><form action="http://127.0.0.1:5000">
   <label for="airline">Airline ID:</label><br>
   <input type="text" id="airline" name="airline" value='''+airline_id+'''><br>
-  <label for="number">Flight Num:</label><br>
+  <label for="number">Flight Number:</label><br>
   <input type="text" id="number" name="number" value='''+flight_number+'''><br><br>
   <input type="submit" value="Submit">
 </form> 
-
 '''
             html+='<p>'
-            html+='Altitude:' + str(flight[4])
+            html+='Altitude: ' + str(flight[4]) + ' feet'
             html+='</p> <p>'
-            html+='Speed:' + str(flight[5])
+            html+='Speed: ' + str(flight[5]) + ' knots'
             html+='</p> <p>'
-            html+='Heading:' + str(flight[3])
+            html+='Heading: ' + str(flight[3]) + ' degrees'
             html+='</p> <p>'
-            html+='Aircraft:' + str(flight[8])
+            html+='Aircraft: ' + airplane_type(str(flight[8]))
             html+='</p> <p>'
-            html+='Latitude:' + str(flight[1])
+            html+='Latitude: ' + str(flight[1])
             html+='</p> <p>'
-            html+='Longitude:' + str(flight[2])
+            html+='Longitude: ' + str(flight[2])
             html+='</p> <p>'
-            html+='Origin:' + str(flight[11])
+            html+='Origin: ' + str(flight[11]) + ', ' + str(airport_name(flight[11]))
             html+='</p> <p>'
-            html+='Destination:' + str(flight[12])
+            html+='Destination: ' + str(flight[12]) + ', ' + str(airport_name(flight[12]))
             html+='</p></div>'
         except:
             html ='''<!DOCTYPE html>
@@ -68,7 +94,7 @@ def get_html():
             html+='''<form action="http://127.0.0.1:5000">
   <label for="airline">Airline ID:</label><br>
   <input type="text" id="airline" name="airline"><br>
-  <label for="number">Flight Num:</label><br>
+  <label for="number">Flight Number:</label><br>
   <input type="text" id="number" name="number"><br><br>
   <input type="submit" value="Submit">
 </form> 
@@ -83,12 +109,11 @@ def get_html():
         <form action="http://127.0.0.1:5000">
   <label for="airline">Airline ID:</label><br>
   <input type="text" id="airline" name="airline" value='''+airline_id+'''><br>
-  <label for="number">Flight Num:</label><br>
+  <label for="number">Flight Number:</label><br>
   <input type="text" id="number" name="number" value='''+flight_number+'''><br><br>
   <input type="submit" value="Submit">
 </form> 
 </div>
-
 '''
     html+='</body></html>'
     
